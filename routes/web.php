7d877b9b163385c82f64c029605bba883b29fa11
anhar <fndedek@gmailcom>
2023-03-28 10:59:57 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContentController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\SelamatController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/tutor', function(){
+//     return view('welcome');
+// });
+
+
+
+Route::get('/pengalaman-kuliah', [ContentController::class, 'content'])->name('content_kuliah');
+
+Route::get('/profile/{id?}', [ContactController::class, 'profile'])->name('profile');
+
+Route::get('/student/{id}', [PostController::class, 'index'])->name('student');
+
+Route::get('/selamat', [SelamatController::class, 'selamat'])->name('selamat');
+
+Auth::routes();
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
